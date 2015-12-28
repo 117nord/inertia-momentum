3623135a641fe99d11f547ad1e53573060403968
Stéphane Beaulé <117nord@gmail.com>
2015-12-27 20:26:17 -0500

    centered ship rotation axis

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -14,20 +14,30 @@ window.INERTIA_MOMENTUM.canvas = (function () {
     'use strict';
     var canvas,
         ctx,
-        input;
+        input,
+        angle = 0,
+        hull = {
+            top: {x: 0, y: -40},
+            bleft: {x: -30, y: 20},
+            bright: {x: 30, y: 20},
+            midleft: {x: -15, y: -10},
+            midright: {x: 15, y: -10},
+            midbottom: {x: 0, y: 20}
+        };
 
     function drawShip(x, y, state) {
         ctx.save();
 
         ctx.translate(x, y);
-        //ctx.rotate((Math.PI/180)*90);
+        ctx.rotate(angle);
+        angle += state.rotation * (Math.PI/180)*5;
 
         // hull
         ctx.beginPath();
         ctx.strokeStyle = 'rgb(0, 0, 0)';
-        ctx.moveTo(0, -30);
-        ctx.lineTo(30, 30);
-        ctx.lineTo(-30, 30);
+        ctx.moveTo(0, -40);
+        ctx.lineTo(30, 20);
+        ctx.lineTo(-30, 20);
         ctx.closePath();
         ctx.stroke();
 
@@ -35,9 +45,9 @@ window.INERTIA_MOMENTUM.canvas = (function () {
         if (state.thrust) {
             ctx.beginPath();
             ctx.strokeStyle = 'rgb(255, 0, 0)';
-            ctx.moveTo(-10, 30);
-            ctx.lineTo(0, 40);
-            ctx.lineTo(10, 30);
+            ctx.moveTo(-10, 20);
+            ctx.lineTo(0, 30);
+            ctx.lineTo(10, 20);
             ctx.stroke();
         }
 
